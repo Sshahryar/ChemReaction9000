bb1a9eb05e7c7b8956a67639d9dc246e7f07d5b8
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-10-04 19:56:14 -0400

    Update Main.java

--- src/main/java/chemistryapp/model/view/controller/app/utils/Main.java
+++ src/main/java/chemistryapp/model/view/controller/app/utils/Main.java
@@ -1,1 +1,11 @@
+package chemistryapp;
 
+public class Main {
+
+    public static void main(String[] args) {
+        // ChemHelper9000 entry point
+
+        // Placeholder welcoming message
+        System.out.println("Welcome to ChemHelper9000!");
+    }
+}
