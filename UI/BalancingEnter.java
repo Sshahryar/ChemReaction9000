fa86a5361f6934379109db2575fd4576b6b43fa7
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-12-23 23:04:57 -0500

    Created BalancingEnter.fxml and BalancingEnterController.java as corresponding files to BalancingEnter.java
    Still incomplete however.

--- UI/BalancingEnter.java
+++ UI/BalancingEnter.java
@@ -1,57 +1,23 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class BalancingEnter extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+        // Load the FXML file
+        Parent root = FXMLLoader.load(getClass().getResource("BalancingEnter.fxml"));
+
+        // Set up the stage
         primaryStage.setTitle("Chemical Reaction Simulator");
-
-        // Creation of UI elements
-        Label titleLabel = new Label("Enter Chemical Reaction:");
-        TextField reactionInput = new TextField();
-        Button balanceButton = new Button("Balance");
-        TextArea resultTextArea = new TextArea();
-
-        // Balance button placeholder
-        balanceButton.setOnAction(e -> {
-            // Calls the method for getting reaction input
-            String reaction = reactionInput.getText();
-            String balancedReaction = balanceReaction(reaction);
-            resultTextArea.setText("Balanced Reaction:\n" + balancedReaction);
-        });
-
-        // Grid layout based alignment
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.add(titleLabel, 0, 0, 2, 1);
-        grid.add(reactionInput, 0, 1, 2, 1);
-        grid.add(balanceButton, 0, 2, 2, 1);
-        grid.add(resultTextArea, 0, 3, 2, 1);
-
-        // Create a scene and set it on the stage
-        Scene scene = new Scene(grid, 400, 300);
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
 
-    // Input reaction model later
-    private String balanceReaction(String reaction) {
-        // Where the reaction model will be based
-        // Base input for now
-        return reaction;
+    public static void main(String[] args) {
+        launch(args);
     }
-}   
+}
---
+++ UI/BalancingEnterController.java
@@ -0,0 +1,6 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+public class BalancingEnterController {
+}
