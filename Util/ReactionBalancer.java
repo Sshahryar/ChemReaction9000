501c1bc0ddb23989ae9c85efe0e9f7271426ec8e
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-01-03 19:08:23 -0500

    Created new package: Util. Also created ReactionBalancer file.
    Need to research advanced algorithms which will be suitable for balancing advanced chemical reactions.

---
+++ Util/ReactionBalancer.java
@@ -0,0 +1,11 @@
+package Util;
+
+public class ReactionBalancer {
+    public static String balance(String reaction) {
+        //This is where the reactants and products get split
+        String reactantsStr = parts [0].trim();
+        String productsStr = parts [1].trim();
+
+        return reaction;
+    }
+}
