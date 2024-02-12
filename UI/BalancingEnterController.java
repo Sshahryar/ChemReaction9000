939ede51f4a17819fa3fb207e8f7d359f9ca6137
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-02-11 22:01:52 -0500

    Implemented interface functionality for selecting reaction type.

--- UI/BalancingEnterController.java
+++ UI/BalancingEnterController.java
@@ -1,34 +1,62 @@
-package UI;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class BalancingEnterController {
 
-    @FXML
-    private TextField reactionInput;
+    private TextField reactionInput = new TextField();
+    private ComboBox<String> reactionTypeComboBox = new ComboBox<>();
+    private Button balanceButton = new Button("Balance Reaction");
+    private TextArea resultTextArea = new TextArea();
 
-    @FXML
-    private Button balanceButton;
-
-    @FXML
-    private TextArea resultTextArea;
-
-    @FXML
-    private void initialize() {
-        balanceButton.setOnAction(this::balanceReaction);
+    public BalancingEnterController() {
+        initialize();
     }
 
-    private void balanceReaction(ActionEvent event) {
+    private void initialize() {
+        reactionTypeComboBox.getItems().addAll("Synthesis", "Decomposition", "Ionic");
+        balanceButton.setOnAction(event -> balanceReaction());
+    }
+
+    private void balanceReaction() {
         String reaction = reactionInput.getText();
-        String balancedReaction = balanceReaction(reaction);
+        String reactionType = reactionTypeComboBox.getValue();
+        String balancedReaction = "";
+
+        switch (reactionType) {
+            case "Synthesis":
+                balancedReaction = balanceSynthesis(reaction);
+                break;
+            case "Decomposition":
+                balancedReaction = balanceDecomposition(reaction);
+                break;
+            case "Ionic":
+                balancedReaction = balanceIonic(reaction);
+                break;
+            default:
+                break;
+        }
+
         resultTextArea.setText("Balanced Reaction:\n" + balancedReaction);
     }
 
-    private String balanceReaction(String reaction) {
-        return reaction; 
+    private String balanceSynthesis(String reaction) {
+        return reaction;
+    }
+
+    private String balanceDecomposition(String reaction) {
+        return reaction;
+    }
+
+    private String balanceIonic(String reaction) {
+        return reaction;
+    }
+
+    public VBox getView() {
+        VBox view = new VBox(10);
+        view.getChildren().addAll(reactionInput, reactionTypeComboBox, balanceButton, resultTextArea);
+        return view;
     }
 }
