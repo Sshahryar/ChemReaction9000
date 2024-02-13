9b8ea8f235dee1db2240604aef0831ef2a9e9eb2
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-02-12 23:18:19 -0500

    Initialized the files for visualizing the chemical reactions.

---
+++ UI/BalancingVisualizationController.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class BalancingVisualizationController {
+
+    @FXML
+    private VBox root;
+
+    @FXML
+    private Label titleLabel;
+
+    public void initialize() {
+    }
+
+}
