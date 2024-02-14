5442b5c96f9e22a48b4c3747d2f0759bf327b3e3
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-02-13 22:54:28 -0500

    Updated UI with input field labels.

--- UI/BalancingEnterController.java
+++ UI/BalancingEnterController.java
@@ -1,5 +1,6 @@
+package UI;
+
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -7,7 +8,6 @@ import javafx.scene.layout.VBox;
 public class BalancingEnterController {
 
     private TextField reactionInput = new TextField();
-    private ComboBox<String> reactionTypeComboBox = new ComboBox<>();
     private Button balanceButton = new Button("Balance Reaction");
     private TextArea resultTextArea = new TextArea();
 
@@ -16,47 +16,23 @@ public class BalancingEnterController {
     }
 
     private void initialize() {
-        reactionTypeComboBox.getItems().addAll("Synthesis", "Decomposition", "Ionic");
         balanceButton.setOnAction(event -> balanceReaction());
     }
 
     private void balanceReaction() {
         String reaction = reactionInput.getText();
-        String reactionType = reactionTypeComboBox.getValue();
-        String balancedReaction = "";
-
-        switch (reactionType) {
-            case "Synthesis":
-                balancedReaction = balanceSynthesis(reaction);
-                break;
-            case "Decomposition":
-                balancedReaction = balanceDecomposition(reaction);
-                break;
-            case "Ionic":
-                balancedReaction = balanceIonic(reaction);
-                break;
-            default:
-                break;
-        }
-
+        String balancedReaction = balanceReaction(reaction);
         resultTextArea.setText("Balanced Reaction:\n" + balancedReaction);
     }
 
-    private String balanceSynthesis(String reaction) {
-        return reaction;
-    }
-
-    private String balanceDecomposition(String reaction) {
-        return reaction;
-    }
-
-    private String balanceIonic(String reaction) {
+    private String balanceReaction(String reaction) {
         return reaction;
     }
 
     public VBox getView() {
         VBox view = new VBox(10);
-        view.getChildren().addAll(reactionInput, reactionTypeComboBox, balanceButton, resultTextArea);
+        view.getChildren().addAll(reactionInput, balanceButton, resultTextArea);
         return view;
     }
 }
+
