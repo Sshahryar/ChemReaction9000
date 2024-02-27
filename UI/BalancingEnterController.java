4e62ab41f729279bbed5f19246d5c39ba2d10efe
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-02-26 22:29:13 -0500

    Revamped BalancingEnterController for better user experience: added tooltips, keyboard shortcuts, and button icons, along with refining code organization

--- UI/BalancingEnterController.java
+++ UI/BalancingEnterController.java
@@ -3,6 +3,9 @@ package UI;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class BalancingEnterController {
@@ -19,6 +22,17 @@ public class BalancingEnterController {
     private void initialize() {
         balanceButton.setOnAction(event -> balanceReaction());
         clearButton.setOnAction(event -> clearFields());
+        
+        balanceButton.setTooltip(createTooltip("Click to balance the chemical reaction."));
+        clearButton.setTooltip(createTooltip("Click to clear input and result fields."));
+        
+        balanceButton.setMnemonicParsing(true);
+        balanceButton.setMnemonicKeyCode('B');
+        clearButton.setMnemonicParsing(true);
+        clearButton.setMnemonicKeyCode('C');
+        
+        balanceButton.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/balance.png"))));
+        clearButton.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/clear.png"))));
     }
 
     private void balanceReaction() {
@@ -41,13 +55,24 @@ public class BalancingEnterController {
     }
 
     private String balanceChemicalReaction(String reaction) {
- 
         return "Balanced Chemical Reaction for " + reaction;
+    }
+    
+    private Tooltip createTooltip(String text) {
+        Tooltip tooltip = new Tooltip();
+        tooltip.setText(text);
+        return tooltip;
     }
 
     public VBox getView() {
         VBox view = new VBox(10);
-        view.getChildren().addAll(reactionInput, balanceButton, clearButton, resultTextArea);
+        view.getChildren().addAll(reactionInput, createButtonBar(), resultTextArea);
         return view;
+    }
+    
+    private HBox createButtonBar() {
+        HBox buttonBar = new HBox(10);
+        buttonBar.getChildren().addAll(balanceButton, clearButton);
+        return buttonBar;
     }
 }
