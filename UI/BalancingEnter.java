dc05654a11850644df2d3de3803382d896ef5c6f
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-05-08 22:28:01 -0400

    relocated files

---
+++ UI/BalancingEnter.java
@@ -0,0 +1,27 @@
+    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BalancingEnter extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = loadFXML("BalancingEnter.fxml");
+
+        primaryStage.setTitle("Chemical Reaction Balancer");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+    private Parent loadFXML(String fxmlFileName) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
+        return loader.load();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
