e29b620864b942f3740f98d6088fa733117a0639
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-10-17 00:05:28 -0400

    testing, recurring issue with missing classpaths
    javaFX modules correct

--- launch/BalancingEnter.java
+++ launch/BalancingEnter.java
@@ -1,27 +1,30 @@
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-class BalancingEnter extends Application {
-
+public class BalancingEnter extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        System.out.println("Starting application...");
         Parent root = loadFXML("BalancingEnter.fxml");
-
+        System.out.println("FXML loaded successfully.");
         primaryStage.setTitle("Chemical Reaction Balancer");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
+        System.out.println("Application launched.");
     }
 
     private Parent loadFXML(String fxmlFileName) throws IOException {
+        System.out.println("Loading FXML: " + fxmlFileName);
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName));
         return loader.load();
     }
 
     public static void main(String[] args) {
+        System.out.println("Launching application...");
         launch(args);
+        System.out.println("Main method finished.");
     }
 }
