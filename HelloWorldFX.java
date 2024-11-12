a0ff7683ae751fb777830d1f82b15a0e2938ca6a
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-11-12 00:11:53 -0500

    Created test file to further investigate graphics pipeline issue

---
+++ HelloWorldFX.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorldFX extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello World from JavaFX!");
+        Scene scene = new Scene(new StackPane(label), 300, 200);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
