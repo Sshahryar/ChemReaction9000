c14a14843188dcd83731427e3cb22f519ea8ddd1
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-11-22 00:08:31 -0500

    Included JavaFX native library paths directly to batch file

--- runJavaFX.bat
+++ runJavaFX.bat
@@ -1,7 +1,9 @@
 @echo off
 set PATH_TO_FX=C:/Users/hussa/Downloads/openjfx17/javafx-sdk-17.0.13/lib
 set PATH_TO_FX_BIN=C:/Users/hussa/Downloads/openjfx17/javafx-sdk-17.0.13/bin
-set JAVA_OPTS=-Dprism.forceGPU=false -Dprism.order=sw -Dprism.verbose=true -Djava.library.path=%PATH_TO_FX_BIN%
+set PATH=%PATH%;%PATH_TO_FX_BIN%
+set JAVA_OPTS=-Dprism.forceGPU=false -Dprism.order=sw -Dprism.verbose=true
 javac --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml -d . BalancingEnter.java
 java %JAVA_OPTS% --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml -cp . BalancingEnter
+
 
