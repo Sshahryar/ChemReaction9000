a2d0dd8f42c7765319d0e32acb3013240402a3f4
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-11-24 00:08:57 -0500

    Simplified PATH environments

--- runJavaFX.bat
+++ runJavaFX.bat
@@ -2,8 +2,8 @@
 set PATH_TO_FX=C:/Users/hussa/Downloads/openjfx17/javafx-sdk-17.0.13/lib
 set PATH_TO_FX_BIN=C:/Users/hussa/Downloads/openjfx17/javafx-sdk-17.0.13/bin
 set PATH=%PATH%;%PATH_TO_FX_BIN%
-set JAVA_OPTS=-Dprism.forceGPU=false -Dprism.order=sw -Dprism.verbose=true
 javac --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml -d . BalancingEnter.java
-java %JAVA_OPTS% --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml -cp . BalancingEnter
+java --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml -cp . BalancingEnter
+
 
 
