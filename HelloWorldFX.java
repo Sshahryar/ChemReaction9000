74611e23ceeee6f8d3e2b8bcd685b93913d5af9c
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-12-13 00:12:49 -0500

    Modified testing approach and highlighted next steps

--- HelloWorldFX.java
+++ HelloWorldFX.java
@@ -4,10 +4,10 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class TestFX extends Application {
+public class HelloWorldFX extends Application {
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Hello JavaFX!");
+        Label label = new Label("Hello World from JavaFX!");
         Scene scene = new Scene(new StackPane(label), 300, 200);
         stage.setScene(scene);
         stage.show();
@@ -17,4 +17,5 @@ public class TestFX extends Application {
         launch(args);
     }
 }
+
 
